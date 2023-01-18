612422bbe26d22b45926eb58d2159d98eea5613c
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2023-01-18 12:51:51 +0200

    makes the hero responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,11 @@ const Hero = () => {
   return (
       <Section color="bg-pastelYellow" style="flex md:flex-row flex-col items-center mx-auto sm:justify-around justify-center">
        <div className=" flex flex-col">
-         <h1 className="md:text-7xl text-5xl font-semibold md:leading-[90px] sm:leading-[50px]">Manage All <br /> Your Tasks <br /> With Ease</h1>
+         <h1 className="md:text-7xl sm:text-5xl text-4xl font-semibold md:leading-[90px] sm:leading-[50px]">Manage All <br /> Your Tasks <br /> With Ease</h1>
          <p className="text-dimBlack text-lg max-w-xl sm:py-16 py-6">Do more than you could ever dream of in one day, and make your days twice as long by managing your tasks with our App. Sign in and start using it today.</p>
          <button className="self-start font-semibold bg-pastelOrange rounded-md sm:mb-0 mb-6 py-3 px-4">Get Started</button>
        </div>
-        <img src={reminder} alt="reminder" className="w-[400px]" draggable={"false"}/>
+        <img src={reminder} alt="reminder" className="sm:w-[400px] w-[280px]" draggable={"false"}/>
       </Section>
   );
 };
