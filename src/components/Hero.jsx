288c7448d7a7291f401e47d0a61db098478d3e5d
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2023-01-18 12:46:52 +0200

    makes the hero responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,10 @@ import reminder from "../assets/reminder.svg"
 
 const Hero = () => {
   return (
-      <Section color="bg-pastelYellow" style="flex items-center mx-auto justify-around">
+      <Section color="bg-pastelYellow" style="flex md:flex-row flex-col items-center mx-auto justify-around">
        <div className=" flex flex-col">
-         <h1 className="text-7xl font-semibold leading-[90px]">Manage All <br /> Your Tasks <br /> With Ease</h1>
-         <p className="text-dimBlack text-lg max-w-xl py-16">Do more than you could ever dream of in one day, and make your days twice as long by managing your tasks with our App. Sign in and start using it today.</p>
+         <h1 className="md:text-7xl sm:text-5xl text-3xl font-semibold md:leading-[90px] sm:leading-[50px]">Manage All <br /> Your Tasks <br /> With Ease</h1>
+         <p className="text-dimBlack text-lg max-w-xl sm:py-16 py-6">Do more than you could ever dream of in one day, and make your days twice as long by managing your tasks with our App. Sign in and start using it today.</p>
          <button className="self-start font-semibold bg-pastelOrange rounded-md py-3 px-4">Get Started</button>
        </div>
         <img src={reminder} alt="reminder" className="w-[400px]" draggable={"false"}/>
