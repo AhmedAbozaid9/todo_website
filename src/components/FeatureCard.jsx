ad5ee28a5bfd94fb96f80c88ccd16634e9d680f7
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2023-02-04 13:29:01 +0200

    changes the features layout to grid

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const FeatureCard = ({title,content,img}) => {
   return (
-    <div className="bg-white p-4 md:w-[370px] w-[260px] md:h-[200px] h-[150px] relative">
-      <h3 className="text-2xl font-normal">{title}</h3>
-      <p className="text-dimBlack">{content}</p>
-      <img draggable={"false"} src={img} alt={title} className="md:w-[45px] w-[28px] absolute bottom-4 right-4"/>
+    <div className="bg-white p-4 lg:w-[370px] lg:h-[200px] md:w-[260px] md:h-[150px] w-[220px] h-[120px] relative">
+      <h3 className="text-2xl font-normal md:text-xl">{title}</h3>
+      <p className="text-dimBlack md:text-lg sm:text-sm text-[12px]">{content}</p>
+      <img draggable={"false"} src={img} alt={title} className="lg:w-[45px] md:w-[28px] w-[23px] absolute bottom-4 right-4"/>
     </div>
   );
 };
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -7,11 +7,11 @@ import superWoman from "../assets/superWoman.svg"
 const Features = () => (
   <Section color="bg-pastelOrange" style="flex flex-col justify-around items-center">
     <h1 className=" text-3xl sm:text-4xl font-semibold text-white"><span className="border-b-4 border-b-white font-robotoSlab">Powered</span> by Technology</h1>
-    <div className="flex flex-col md:flex-row justify-center items-center">
-      <div className="grid grid-cols-2 grid-rows-2 gap-4">
+    <div className="grid md:grid-rows-1 md:grid-cols-2 grid-rows-2 grid-cols-1 justify-center items-center">
+      <div className="flex gap-2 lg:min-w-[800px] md:min-w-[550px] min-w-[450px] flex-wrap">
         {features.map(feature => <FeatureCard key={feature.id} {...feature} />)}
       </div>
-      <img src={superWoman} alt="superWoman" className="w-[500px] md:w-[700px]"/>
+      <img src={superWoman} alt="superWoman" className="sm-[500px] md:w-[700px] "/>
     </div>
   </Section>
 );
