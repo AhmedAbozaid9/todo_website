76bf37e63745f05902989115b1b9dee2306e896f
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2023-02-04 14:07:26 +0200

    changes the features layout to grid

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -8,7 +8,7 @@ const Features = () => (
   <Section color="bg-pastelOrange" style="flex flex-col justify-around items-center">
     <h1 className=" text-2xl sm:text-4xl font-semibold text-white"><span className="border-b-4 border-b-white font-robotoSlab">Powered</span> by Technology</h1>
     <div className="grid md:grid-rows-1 md:grid-cols-2 grid-rows-2 grid-cols-1 justify-center items-start sm:items-center">
-      <div className="flex gap-2 lg:min-w-[800px] md:min-w-[550px] min-w-[450px] flex-wrap">
+      <div className="flex gap-2 lg:min-w-[800px] md:min-w-[550px] sm:min-w-[450px] min-w-[350px] overflow-x-hidden flex-wrap">
         {features.map(feature => <FeatureCard key={feature.id} {...feature} />)}
       </div>
       <img src={superWoman} alt="superWoman" className="sm-[500px] md:w-[700px] "/>
